f7aaefaaf8d7c86cfc13430b626d5d59cb9a8ec6
Park Dongik <34857208+Dongik-Park@users.noreply.github.com>
2019-01-04 10:13:00 +0900

    6719. 성수의 프로그래밍 강좌 시청

---
+++ swexpert_6719.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, K;
+double levels[10000];
+
+double solve(int idx, double level, int cnt) {
+	double result = 0;
+	for (int i = N - K; i < N; ++i) // 가장 뒤 - K번부터 가장 뒤까지의 강좌를 수강한다.
+		result = (levels[i] + result) / 2;
+	return result;
+}
+
+int main()
+{
+	int test_case;
+	int T;
+
+	cin >> T;
+
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		cin >> N >> K;
+		for (int i = 0; i < N; ++i)
+			cin >> levels[i];
+
+		// 오름차순으로 정렬한다. ( 낮은 수준부터 수강하는게 유리하다 )
+		sort(levels, levels + N);
+
+		cout << fixed;
+		cout.precision(6);
+		cout << "#" << test_case << " " << solve(0, 0, 0) << endl;
+	}
+	return 0;
+}
