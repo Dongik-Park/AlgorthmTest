e314d478c0d02e43d2b96873a1a943cd505f42ec
Park Dongik <34857208+Dongik-Park@users.noreply.github.com>
2019-03-07 10:35:52 +0900

    1861. 정사각형 방(dp, 성능3배~4배향상)

--- swexpert_1861.java
+++ swexpert_1861.java
@@ -5,78 +5,75 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class swexpert_1861 {
 	static int N;
-	static final int MAX_N = 1000;
-	static int map[][] = new int[MAX_N + 1][MAX_N + 1];
-	static boolean visited[][] = new boolean[MAX_N + 1][MAX_N + 1];
-	static int dx[] = { -1, 1, 0, 0 };
-	static int dy[] = { 0, 0, -1, 1 };
+	static int[][] map;
+	static int[][] dp;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static int dfs(int row, int col) {
+		if (dp[row][col] != 0) {
+			return dp[row][col];
+		} else { // 상하좌우에서 이동 가능할 경우 dfs로 찾는다.
+			int tempOut = 1;
+			int currentValue = map[row][col];
+			if (row - 1 >= 0 && currentValue + 1 == map[row - 1][col]) {
+				tempOut = Math.max(tempOut, dfs(row - 1, col) + 1);
+			}
+			if (row + 1 < N && currentValue + 1 == map[row + 1][col]) {
+				tempOut = Math.max(tempOut, dfs(row + 1, col) + 1);
+			}
+			if (col - 1 >= 0 && currentValue + 1 == map[row][col - 1]) {
+				tempOut = Math.max(tempOut, dfs(row, col - 1) + 1);
+			}
+			if (col + 1 < N && currentValue + 1 == map[row][col + 1]) {
+				tempOut = Math.max(tempOut, dfs(row, col + 1) + 1);
+			}
+			dp[row][col] = tempOut;
+			return dp[row][col];
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int T = Integer.parseInt(br.readLine().trim());
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc <= T; ++tc) {
+			N = Integer.parseInt(br.readLine());
+			map = new int[N][N];
+			dp = new int[N][N];
 
-		for (int tc = 1; tc <= T; ++tc) {
-			// data input
-			N = Integer.parseInt(br.readLine().trim());
-			for (int i = 0; i < N; ++i) {
-				String s[] = br.readLine().trim().split(" ");
-				for (int j = 0; j < N; ++j) {
-					map[i][j] = Integer.parseInt(s[j]);
-					visited[i][j] = false;
+			for (int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+				for (int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			// solve
-			int res[] = solve();
-			// data output
-			bw.write("#" + tc + " " + res[0] + " " + res[1] + "\n");
+			// 1. 이동가능 경로 중 가장 긴 경로 찾는다.
+			int maxValue = Integer.MIN_VALUE;
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					maxValue = Math.max(maxValue, dfs(i, j));
+				}
+			}
+			// 2. 가장 긴 경로 중 가장 작은 수를 찾는다.
+			int out = Integer.MAX_VALUE;
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (dp[i][j] == maxValue) {
+						out = Math.min(out, map[i][j]);
+					}
+				}
+			}
+			bw.write("#" + tc + " " + out + " " + maxValue + "\n");
 		}
-
 		bw.flush();
-		bw.close();
 		br.close();
+		bw.close();
 	}
 
-	private static int[] solve() {
-		int res[] = {N * N, 0};
-		for (int i = 0; i < N; ++i) {
-			for(int j = 0; j < N; ++j) {
-				for(int dir = 0; dir < dx.length; ++dir) {
-					int nr = i + dx[dir];
-					int nc = j + dy[dir];
-					if(nr < 0 || nr >= N || nc < 0 || nc >= N || visited[nr][nc] ||map[nr][nc] - map[i][j] != 1) 
-						continue;
-					visited[i][j] = true;
-					int cur = dfs(nr, nc, 1);
-					if(res[1] < cur) { // 이동 횟수가 많은 경우
-						res[0] = map[i][j];
-						res[1] = cur;
-					}
-					else if(res[1] == cur && map[i][j] < res[0]) { // 이동 횟수가 현재값이랑 같고 방번호가 작을 경우
-						res[0] = map[i][j];
-					}
-					visited[i][j] = false;
-				}
-			}
-		}
-		return res;
-	}
-
-	private static int dfs(int i, int j, int res) {
-		++res;
-		for(int dir = 0; dir < dx.length; ++dir) {
-			int nr = i + dx[dir];
-			int nc = j + dy[dir];
-			if(nr < 0 || nr >= N || nc < 0 || nc >= N || visited[nr][nc] || map[nr][nc] - map[i][j] != 1) 
-				continue;
-			visited[i][j] = true;
-			res = dfs(nr, nc, res);
-			visited[i][j] = false;
-		}
-		return res;
-	}
 }
